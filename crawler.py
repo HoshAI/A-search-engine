e346b258d4facf83f884b63691e8ff14fba19893
Ayodeji Emmanuel <72923834+HoshAI@users.noreply.github.com>
2022-06-16 18:56:16 -0700

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-start_url = 'https://stackoverflow.com/questions/'
+start_url = 'https://stackoverflow.com/questions/?tab=newest&pagesize=50'
 
 response = requests.get(start_url)
 content = BeautifulSoup(response.text, 'lxml')
 links = content.findAll('a', {'class': 'question-hyperlink'})
 
 for link in links:
-  print(link['href'], link.text)
+  if 'https://' not in link['href']:
+  print(link['href'])
