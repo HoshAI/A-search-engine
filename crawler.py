458c924f0d621aefd0ff1646a69be8842776c815
Ayodeji Emmanuel <72923834+HoshAI@users.noreply.github.com>
2022-06-16 18:48:25 -0700

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,1 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
 
+start_url = 'https://stackoverflow.com/questions/'
+
+response = requests.get(start_url)
+content = BeautifulSoup(response.text, 'lxml')
+links = content.findAll('a', {'class': 'question-hyperlink'})
+
+for link in links:
+  print(link['href'], link.text)
